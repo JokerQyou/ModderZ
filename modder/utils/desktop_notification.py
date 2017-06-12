6994a94f55380c7a0eafeaab28fe42bc29db8e4d
Joker Qyou <Joker.Qyou@gmail.com>
2017-06-12 23:09:05 +0800

    Add icon for desktop notifications

--- modder/utils/desktop_notification.py
+++ modder/utils/desktop_notification.py
@@ -1,7 +1,23 @@
 # coding: utf-8
+import os.path
 import platform
+import sys
+
+if getattr(sys, 'frozen', False):
+    desktop_icon = os.path.abspath(
+        os.path.join(
+            os.path.dirname(sys.executable), 'resources', 'icons8-Module-128.png'
+        )
+    )
+else:
+    desktop_icon = os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__), 'gui', 'resources', 'icons8-Module-128.png'
+        )
+    )
 
 if platform.system() == 'Darwin':
+    from AppKit import NSImage
     from Foundation import NSUserNotificationDefaultSoundName
     import objc
 
@@ -14,18 +30,24 @@ if platform.system() == 'Darwin':
         notification = NSUserNotification.alloc().init()
         notification.setTitle_(title.decode('utf-8'))
         notification.setInformativeText_(text.decode('utf-8'))
+        notification.setValue_forKey_(NSImage.alloc().initWithContentsOfFile_(desktop_icon), '_identityImage')
 
         if sound:
             notification.setSoundName_(NSUserNotificationDefaultSoundName)
 
-        center = NSUserNotificationCenter.defaultUserNotificationCenter()
-        center.deliverNotification_(notification)
+        NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification_(notification)  # noqa
 
 elif platform.system() == 'Windows':
+    from win10toast import ToastNotifier
+
     def desktop_notify(text, title=None, sound=False):
         title = title or 'Modder'
 
-        pass
+        ToastNotifier().show_toast(
+            title.decode('utf-8'),
+            text.decode('utf-8'),
+            icon_path=desktop_icon
+        )
 
 elif platform.system() == 'Linux':
     def desktop_notify(text, title=None, sound=False):
