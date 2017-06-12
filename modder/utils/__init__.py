4f89cf6eca40c9739d1e93f781ce8dc3867e625f
Joker Qyou <Joker.Qyou@gmail.com>
2017-06-12 22:05:41 +0800

    Move utils to modder.utils

---
+++ modder/utils/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+from .log_utils import get_logger
+from .desktop_notification import desktop_notify
+
+
+__all__ = ['get_logger', 'desktop_notify', ]
--- utils/__init__.py
+++
@@ -1,2 +0,0 @@
-# coding: utf-8
-from .log_utils import get_logger
