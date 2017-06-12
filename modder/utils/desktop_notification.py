a0673604e2bb6ab1bf4915be97055e4cb1d86f42
Joker Qyou <Joker.Qyou@gmail.com>
2017-06-12 22:06:13 +0800

    Add desktop notification util

---
+++ modder/utils/desktop_notification.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+import platform
+
+if platform.system() == 'Darwin':
+    from Foundation import NSUserNotificationDefaultSoundName
+    import objc
+
+    NSUserNotification = objc.lookUpClass('NSUserNotification')
+    NSUserNotificationCenter = objc.lookUpClass('NSUserNotificationCenter')
+
+    def desktop_notify(text, title='Modder', sound=False):
+        notification = NSUserNotification.alloc().init()
+        notification.setTitle_(title.decode('utf-8'))
+        notification.setInformativeText_(text.decode('utf-8'))
+
+        if sound:
+            notification.setSoundName_(NSUserNotificationDefaultSoundName)
+
+        center = NSUserNotificationCenter.defaultUserNotificationCenter()
+        center.deliverNotification_(notification)
+
+elif platform.system() == 'Windows':
+    def desktop_notify(text, title='Modder', sound=False):
+        pass
+
+elif platform.system() == 'Linux':
+    def desktop_notify(text, title='Modder', sound=False):
+        pass
