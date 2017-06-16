a6dff3c434ba464bc27725dd9f1f0d1ba94f9e77
Joker Qyou <Joker.Qyou@gmail.com>
2017-06-16 12:42:40 +0800

    Use Process.Creation event to kill blacklisted process more quickly in kill_blacklisted_process mod

--- modder/mods/kill_blacklisted_process.py
+++ modder/mods/kill_blacklisted_process.py
@@ -15,23 +15,40 @@ else:
 
 @on('Modder.Started')
 @on('Timer.Interval.Minute')
+@on('Process.Created')
 def kill_blacklisted_processes(event):
     if not blacklist:
         return
 
-    targets = filter(lambda p: p.name() in blacklist, psutil.process_iter())
-    killed_count = 0
-    total_count = len(targets)
+    if event.name == 'Process.Created':
+        if event.process_name in blacklist:
+            try:
+                psutil.Process(pid=event.pid).kill()
+            except:
+                notify(
+                    '未能杀死黑名单进程 {:s}。'.format(event.process_name),
+                    title='进程黑名单'
+                )
+            else:
+                notify(
+                    '实时杀死黑名单进程 {:s}。'.format(event.process_name),
+                    title='进程黑名单'
+                )
+    else:
+        targets = filter(lambda p: p.name() in blacklist, psutil.process_iter())
+        killed_count = 0
+        total_count = len(targets)
 
-    for process in targets:
-        try:
-            process.kill()
-        except:
-            pass
-        else:
-            killed_count += 1
+        for process in targets:
+            try:
+                process.kill()
+            except:
+                pass
+            else:
+                killed_count += 1
 
-    notify(
-        '杀死了 {:d} 个黑名单进程，共检测到 {:d} 个。'.format(killed_count, total_count),
-        title='进程黑名单'
-    )
+        if any([killed_count, total_count]):
+            notify(
+                '杀死了 {:d} 个黑名单进程，共检测到 {:d} 个。'.format(killed_count, total_count),
+                title='进程黑名单'
+            )
